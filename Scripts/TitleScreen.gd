15221c2c571cbdb62ce649935a11fb4cf3314427
LatidoReMe <30684769+LatidoReMe@users.noreply.github.com>
2024-11-11 00:31:03 -0800

    Minor repositioning

--- Scripts/TitleScreen.gd
+++ Scripts/TitleScreen.gd
@@ -8,4 +8,5 @@ extends Node
 func _ready() -> void:
 	load.pressed.connect(get_tree().change_scene_to_file.bind("res://Scenes/game_background.tscn"))
 	new.pressed.connect(get_tree().change_scene_to_file.bind("res://Scenes/NewGame.tscn"))
+	settings.pressed.connect(print.bind("Coming soon to fish. near you."))
 	quit.pressed.connect(get_tree().quit)
