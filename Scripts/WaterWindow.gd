8ce498b4dd781d7dadc3d19840906b4603ff696a
LatidoReMe <30684769+LatidoReMe@users.noreply.github.com>
2024-11-14 05:11:39 -0800

    working on pond

--- Scripts/WaterWindow.gd
+++ Scripts/WaterWindow.gd
@@ -22,7 +22,19 @@ var deep_water_bodies: Array[String] = [
   "Well"
 ]
 
-func _ready():
-	# Setup drain (x button)
-	self.close_requested.connect(self.queue_free)
+var old_size : Vector2i = Vector2i(100, 100)
 
+func _init(_name="BodyofWater", _title="Pond", _scene=null) -> void:
+  super(_name, _title, _scene)
+  position = Vector2i(570, 500)
+  min_size = Vector2i(10,10)
+  size = Vector2i(100, 100)
+
+func _ready() -> void:
+  # Setup drain (x button)
+  self.close_requested.connect(self.queue_free)
+  self.size_changed.connect(_resized)
+
+func _resized() -> void:
+  #while mouse pressed down, nothing?
+  print("window resized")
